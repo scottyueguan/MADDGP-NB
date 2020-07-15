8fc1abb52d685adf30619f4a6d5e0f63fc0285e4
GammaYankee <yguan44@gatech.edu>
2020-07-15 08:18:22 -0400

    plot performance level-k

--- MADDPG/analysis/plot_performance.py
+++ MADDPG/analysis/plot_performance.py
@@ -2,41 +2,45 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pickle
 
-level_0_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_agrewards.pkl", 'rb'))
+max_level = 2
 
-level_1_defender_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_agrewards_level_1_defender.pkl","rb"))
-level_1_attacker_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_agrewards_level_1_attacker.pkl","rb"))
-
-level_2_defender_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_level_2_defender_agrewards.pkl","rb"))
-level_2_attacker_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_level_2_attacker_agrewards.pkl","rb"))
-
-
+level_0_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_level_0_attacker_agrewards.pkl", 'rb'))
 level_0_attacker_rewards = level_0_agrewards[0::2]
 level_0_defender_rewards = level_0_agrewards[1::2]
-level_1_attacker_rewards = level_1_attacker_agrewards[0::2]
-level_1_defender_rewards = level_1_defender_agrewards[1::2]
-level_2_attacker_rewards = level_2_attacker_agrewards[0::2]
-level_2_defender_rewards = level_2_defender_agrewards[1::2]
+
+plt.figure(figsize=(10,4))
 
 x_axis_label = '# of 1000 Episode Batches'
 plt.subplot(121)
 y_axis_label = 'Attacker Reward'
 plt.plot(level_0_attacker_rewards, label="MADDPG")
-plt.plot(level_1_attacker_rewards, label="Level-1 Attacker")
-plt.plot(level_2_attacker_rewards, label="Level-2 Attacker")
 plt.ylabel(y_axis_label)
 plt.xlabel(x_axis_label)
 plt.title("Attacker Rewards")
-plt.legend()
+
 
 plt.subplot(122)
 y_axis_label = 'Defender Reward'
 plt.plot(level_0_defender_rewards, label="MADDPG")
-plt.plot(level_1_defender_rewards, label="Level-1 Defender")
-plt.plot(level_2_defender_rewards, label="Level-2 Defender")
 plt.ylabel(y_axis_label)
 plt.xlabel(x_axis_label)
 plt.title("Defender Rewards")
 plt.legend()
+
+
+for k in range(1, max_level+1):
+    level_k_defender_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_level_{}_defender_agrewards.pkl".format(k),"rb"))
+    level_k_attacker_agrewards = pickle.load(open("../level_k_performance_data/maddpg_hvt_1v1_level_{}_attacker_agrewards.pkl".format(k),"rb"))
+
+    level_k_attacker_rewards = level_k_attacker_agrewards[0::2]
+    level_k_defender_rewards = level_k_defender_agrewards[1::2]
+
+    plt.subplot(121)
+    plt.plot(level_k_attacker_rewards, label="Level-{} Attacker".format(k))
+    plt.legend()
+
+    plt.subplot(122)
+    plt.plot(level_k_defender_rewards, label="Level-{} Defender".format(k))
+    plt.legend()
 
 plt.show()
